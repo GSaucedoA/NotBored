a019c1fa3ed07f1eb26256d2ae924134d4d7e900
GSaucedo <g.saucedo.kd@gmail.com>
2021-10-07 19:20:01 -0500

    Get values from shared preferences and set them to custom toolbar

--- app/src/main/java/dev/cynomys/notbored/SuggestionActivity.kt
+++ app/src/main/java/dev/cynomys/notbored/SuggestionActivity.kt
@@ -1,11 +1,29 @@
 package dev.cynomys.notbored
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import dev.cynomys.notbored.databinding.ActivitySuggestionBinding
 
 class SuggestionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySuggestionBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_suggestion)
+
+        binding = ActivitySuggestionBinding.inflate(layoutInflater)
+        sharedPreferences = getSharedPreferences(Const.SHARED_NAME, MODE_PRIVATE)
+
+        setContentView(binding.root)
+        binding.customToolbar.apply {
+            toolbarTitle.text = sharedPreferences.getString(Const.SUGGESTION_TITLE, "Where do you come?")
+            toolbarShuffle.visibility = View.INVISIBLE
+            toolbarBack.visibility = View.VISIBLE
+            toolbarBack.setOnClickListener {
+                onBackPressed()
+            }
+        }
+
     }
 }
