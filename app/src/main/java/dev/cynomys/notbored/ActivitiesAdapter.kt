a622311b468f9a0c8da1a9e7c39c189e37ca8cef
GSaucedo <g.saucedo.kd@gmail.com>
2021-10-07 19:21:24 -0500

    Added click listener to each item

--- app/src/main/java/dev/cynomys/notbored/ActivitiesAdapter.kt
+++ app/src/main/java/dev/cynomys/notbored/ActivitiesAdapter.kt
@@ -1,12 +1,20 @@
 package dev.cynomys.notbored
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
-class ActivitiesAdapter(private val array: Array<String>) :
+class ActivitiesAdapter(
+    private val array: Array<String>,
+    private val sharedPreferences: SharedPreferences,
+    private val context: Context
+) :
     RecyclerView.Adapter<ActivitiesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,6 +26,12 @@ class ActivitiesAdapter(private val array: Array<String>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val headerTitle = array.get(position)
         holder.headerTitle.text = headerTitle
+        holder.headerContainer.setOnClickListener {
+            sharedPreferences.edit().apply {
+                putString(Const.SUGGESTION_TITLE, headerTitle)
+            }.commit()
+            context.startActivity(Intent(context, SuggestionActivity::class.java))
+        }
     }
 
     override fun getItemCount(): Int {
@@ -26,5 +40,6 @@ class ActivitiesAdapter(private val array: Array<String>) :
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val headerTitle = view.findViewById<TextView>(R.id.header_title)
+        val headerContainer = view.findViewById<ConstraintLayout>(R.id.header_container)
     }
 }
