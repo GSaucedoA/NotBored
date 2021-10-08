c72ba0fe45a84c66468ab5ff811c08d94eaf279d
GSaucedo <g.saucedo.kd@gmail.com>
2021-10-08 18:09:58 -0500

    Added functionality to try another button

--- app/src/main/java/dev/cynomys/notbored/SuggestionActivity.kt
+++ app/src/main/java/dev/cynomys/notbored/SuggestionActivity.kt
@@ -27,11 +27,56 @@ class SuggestionActivity : AppCompatActivity() {
             }
         }
 
-        binding.suggestionTitle.text = sharedPreferences.getString(Const.SUGGESTION, "Error")
-        binding.participantsCount.text = sharedPreferences.getString(Const.PARTICIPANTS_COUNT, "0")
+        var category: String? = null
         if (title == "Random") {
             binding.categoryLayout.visibility = View.VISIBLE
-            binding.categoryText.text = resources.getStringArray(R.array.activitiesList).random()
+            category = resources.getStringArray(R.array.activitiesList).random()
         }
+        setInfo(
+            sharedPreferences.getString(Const.SUGGESTION, "Error"),
+            sharedPreferences.getString(Const.PARTICIPANTS_COUNT, "0"),
+            getPrice(0f),
+            category
+        )
+        binding.tryAnother.setOnClickListener {
+            tryAnother()
+        }
+    }
+
+    private fun setInfo(
+        suggestionTitle: String?,
+        participantsCount: String?,
+        price: String?,
+        category: String?
+    ) {
+        suggestionTitle?.let {
+            binding.suggestionTitle.text = it
+        }
+        participantsCount?.let {
+            binding.participantsCount.text = it
+        }
+        price?.let {
+            binding.priceString.text = it
+        }
+        category?.let {
+            binding.categoryText.text = it
+        }
+    }
+
+    private fun tryAnother() {
+        setInfo(
+            resources.getStringArray(R.array.activitiesSuggestions).random(),
+            null,
+            null,
+            resources.getStringArray(R.array.activitiesList).random()
+        )
+    }
+
+    private fun getPrice(price: Float): String {
+        var stringPrice = "Free"
+        if (price > 0 && price <= 0.3) stringPrice = "Low"
+        else if (price > 0.3 && price <= 0.6) stringPrice = "medium"
+        else if (price > 0.6) stringPrice = "High"
+        return stringPrice
     }
 }
